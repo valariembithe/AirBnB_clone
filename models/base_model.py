2989af9f9f0edcb618c0629b9ed83e62761dbab7
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-08 06:28:14 +0300

    ✨✨✨ re-created an instance of the BaseModel class using a dict

--- models/base_model.py
+++ models/base_model.py
@@ -28,13 +28,22 @@ class BaseModel:
     provide common attributes and methods.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            if "__class__" in kwargs:
+                kwargs.pop("__class__")
+
+        for key, value in kwargs.items():
+            if key in ['created_at', 'updated_at']:
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
