192d3c10069ae643e2db82d68a30e69f3bb41ed2
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-08 05:23:55 +0300

    ✨✨✨ Implemented BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import uuid
+from datetime import datetime
+
+"""
+This module defines all common attributes/methods for other classes
+"""
+
+
+class BaseModel:
+    """
+    A base class defining common attributes and methods for other classes.
+
+    Public instance attributes:
+    - id (string): A unique identifier assigned when an instance is created.
+    - created_at (datetime): The date and time when an instance is created.
+    - updated_at (datetime): The date and time when an instance is updated.
+
+    Public instance methods:
+    - __str__(self): Returns a string representation of the object.
+    - save(self): Updates the public instance attribute 'updated_at'\
+    with the current datetime.
+    - to_dict(self): Returns a dictionary containing all keys/values\
+    of __dict__ of the instance.
+
+   Usage:
+    The BaseModel class is designed to be inherited by other classes to\
+    provide common attributes and methods.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Returns a string representaton of the BaseModel odject.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the "updated_at" attribute with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel object.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
