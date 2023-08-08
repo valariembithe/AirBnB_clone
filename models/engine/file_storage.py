c7744e508cb121fc908360191094c159ce6b160c
valariembithe <valmbithe691@gmail.com>
2023-08-08 05:02:12 -0700

    Serializes and Deserializes instances

---
+++ models/engine/file_storage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+
+"""
+    
+    this module serializes instances to a JSON file and deserializes 
+    JSON file to instances
+"""
+class FileStorage:
+    """
+    Private class attributes:
+        __file_path: string - path to the JSON file (ex: file.json)
+        __objects: dictionary - empty but will store all objects
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Public method that returns a dictionary
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+         sets in __objects the obj with key <obj class name>.id
+         """
+        obj_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_name, obj.id)] = obj
+
+    def save(self):
+        """
+        serializes __objects to the JSON file __file_path
+        """
+        odict = FileStorage.__objects
+        obj_dict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+         deserializes the JSON file to __objects
+        """
+        try:
+            with open(FileStorage.__file_path) as f:
+                obj_dict = json.load(f)
+                for o in obj_dict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
