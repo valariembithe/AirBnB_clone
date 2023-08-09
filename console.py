1fcf47eac50ac4cabeb4e81fb5fcdcc3cb010e0e
valariembithe <valmbithe691@gmail.com>
2023-08-09 05:36:50 -0700

    Updated file with functions and made classes a private instance

--- console.py
+++ console.py
@@ -2,16 +2,45 @@
 """This is the command interpreter"""
 
 import cmd
+import re
+from shlex import split
 from models.base_model import BaseModel
 from models import storage
+from models.city import City
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    classes = {
+    __classes = {
         'BaseModel': BaseModel,
-        'User': User
+        'User': User,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review,
+        'State': State
     }
 
     def do_quit(self, arg):
@@ -34,6 +63,29 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on an empty line
         """
         pass
+
+    def default(self, arg):
+        """
+        Default behavior for cmd module when input is invalid
+        """
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_create(self, arg):
         """
@@ -61,7 +113,7 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in HBNBCommand.classes:
+        if args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
@@ -82,7 +134,7 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in HBNBCommand.classes:
+        if args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
@@ -105,7 +157,7 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             for key in storage.all():
                 instances.append(str(storage.all()[key]))
-        elif args[0] in HBNBCommand.classes:
+        elif args[0] in HBNBCommand.__classes:
             for key in storage.all():
                 class_name = key.split('.')[0]
                 if class_name == args[0]:
@@ -125,7 +177,7 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in HBNBCommand.classes:
+        if args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return
         if len(args) < 2:
@@ -144,6 +196,18 @@ class HBNBCommand(cmd.Cmd):
         instance = storage.all()[key]
         setattr(instance, args[2], args[3].strip('"'))
         instance.save()
+
+    def do_count(self, arg):
+        """
+        Usage: count <class> or <class>.count()
+        Retrieve the number of instances of a given class.
+        """
+        arg1 = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if arg1[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
 
 if __name__ == '__main__':
