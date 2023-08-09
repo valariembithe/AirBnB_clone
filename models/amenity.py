e8ba5f7b3e09734fd0ad6a0ad4f345742a5ad6f1
valariembithe <valmbithe691@gmail.com>
2023-08-09 05:37:22 -0700

    subclasses of BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines the Amenity class """
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Public class attributes:
+    name: string - empty string
+    """
+
+    name = ""
