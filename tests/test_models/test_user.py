8be224ea6f7443fcb5bfbaeadef142529c5af19a
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-08-15 05:41:20 +0300

    ✨✨✨ implemented test_user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,40 @@
+import unittest
+from models.user import User
+
+class TestUserModel(unittest.TestCase):
+    """
+    Test suite for the User model.
+
+    Each test method focuses on testing a specific attribute of the User model.
+    """
+
+    def test_email_attribute(self):
+        """
+        Test that the email attribute of User is set correctly.
+        """
+        user = User(email="user@example.com")
+        self.assertEqual(user.email, "user@example.com")
+
+    def test_password_attribute(self):
+        """
+        Test that the password attribute of User is set correctly.
+        """
+        user = User(password="securepassword")
+        self.assertEqual(user.password, "securepassword")
+
+    def test_first_name_attribute(self):
+        """
+        Test that the first_name attribute of User is set correctly.
+        """
+        user = User(first_name="John")
+        self.assertEqual(user.first_name, "John")
+
+    def test_last_name_attribute(self):
+        """
+        Test that the last_name attribute of User is set correctly.
+        """
+        user = User(last_name="Doe")
+        self.assertEqual(user.last_name, "Doe")
+
+if __name__ == '__main__':
+    unittest.main()
